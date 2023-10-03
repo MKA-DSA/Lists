0e182f26f4c769a53a9245cb47472c0701f50210
Emily Tompsett <etompsett@mka.org>
2023-10-03 10:16:19 -0400

    Add files via upload

--- ArrayBasedList.java
+++ ArrayBasedList.java
@@ -1,62 +1,54 @@
-
-public class ArrayBasedList implements List {
+import java.util.*;
+import java.lang.reflect.*;
+public class ArrayBasedList<T> implements List<T> {
 	// instance variables
-	private Object[] array;
+	private Comparable<T>[] array;
 	private int size;
 	
 	// constructors
 	public ArrayBasedList() {
-		
+		size = 0;
+		array = (Comparable<T>[]) Array.newInstance(Comparable.class, 10);
 	}
 	
 	public ArrayBasedList(int capacity) {
-		
+		size = 0;
+		array = (Comparable<T>[]) Array.newInstance(Comparable.class, capacity);
 	}
 	
 	// accessors
-	public Object get(int pos) throws IndexOutOfBoundsException {
-		
+	public Comparable<T> get(int pos) throws Exception{
+		if (pos < size) {
+			return array[pos];
+		}
+		throw new IndexOutOfBoundsException();
 	}
 	
-	public int find(Object o) {
-		
+	public int find(Comparable<T> o) {
+		for (int i = 0; i < size; i++) {
+			if (array[i].equals(o)) {
+				return i;
+			}
+		}
+		return -1;
 	}
-	
 	public int length() {
-		
+		return size;
 	}
 	
-	public String toString() {
-		
-	}
-	
-	
-	// mutators
-	public boolean set(Object o, int pos) {
-		
-	}
-	
-	public void add(Object o) {
-		
-	}
-	
-	public void insert(Object o, int pos) throws IndexOutOfBoundsException {
-		
-	}
-	
-	public Object remove(int pos) throws IndexOutOfBoundsException { 
-		
-	}
-	
-	public boolean remove(Object o) {
-		
-	}
-	
+	public String toString() { return null; }
+
+	public boolean set(Comparable<T> o, int pos) { return false; }
+	public void add(Comparable<T> o) {}
+	public void insert(Comparable<T> o, int pos) throws Exception{}
+	public Comparable<T> remove(int pos) throws Exception{ return null; }
+	public boolean remove(Comparable<T> o) { return false; }
 	public void clear() {
-		
+		array = (Comparable<T>[]) Array.newInstance(Comparable.class, 10);
+		size = 0;
 	}
+	public void sort() {}
 	
-	public static void main(String[] args) {
-			
+	public static void main(String[] args) {		
 	}
 }
